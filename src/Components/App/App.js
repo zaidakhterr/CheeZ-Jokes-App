a6d8b9ca397ff3e1b431bedca26d8063312bd908
zaidakhterr <zaidakhter1202@gmail.com>
2019-09-23 12:43:30 +0500

    Initial Commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.css';
+
+import '../../Styles/App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>
+        Joke <i className='fas fa-laugh'></i>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
